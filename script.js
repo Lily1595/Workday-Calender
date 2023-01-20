755802ca4b03810072544d068949caff6470de92
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-20 20:29:51 +0000

    added text box and formatted css to make it transparent

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
  //DISPLAY CURRENT DATE
  $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
 
+//FUNCTION FOR WHEN USER CLICKS SAVE BUTTON 
+$(".saveBtn").on("click", function () {})
+
+//PSEUDOCODE:
+// when you click on the middle column 
+
