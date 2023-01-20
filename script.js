f79b8f2a5e78544b602b2ea3f46b08f32f80dc48
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-20 20:51:07 +0000

    pseudocoding

--- script.js
+++ script.js
@@ -5,8 +5,10 @@
 $(".saveBtn").on("click", function () {})
 
 //PSEUDOCODE:
-// when you click on the middle column 
-
+// when you type text in the middle column, the value of that text needs to be saved to local storage
+// the value of what is written in the text book should only saved to local storage upon an event of clicking the save button, if user hasn't pressed save then their written content won't be saved when they refresh the page 
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed: use the time now function to get the current time, if the current time is in the future then background is set to green, if its current, leave it blank, if its in the past, then set the background to red. 
+// you need to give each row a value, and then do if statements that assess the 'timenow' against the value of the the row... (?)
 
 //FUNCTION TO GET CURRENT TIME - this shows the hour in terms of which hour in 24 hour clock time, so 8pm is hour 20 
 //function timeTracker() {
