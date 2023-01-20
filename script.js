954743a31e06c244601a1923f2835772967ddeb1
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-20 20:41:17 +0000

    added get current time function

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ $(".saveBtn").on("click", function () {})
 //PSEUDOCODE:
 // when you click on the middle column 
 
+
+//FUNCTION TO GET CURRENT TIME - this shows the hour in terms of which hour in 24 hour clock time, so 8pm is hour 20 
+//function timeTracker() {
+let timeNow = moment().hour();
+//}
+//timeTracker();
+console.log(timeNow);
