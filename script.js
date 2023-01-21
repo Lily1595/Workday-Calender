d23529d81e9b12382f1162071c189d3b411784d5
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-21 13:41:34 +0000

    working on function that occurs when swve button is clicked

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
  $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
 
 //FUNCTION FOR WHEN USER CLICKS SAVE BUTTON 
-$(".saveBtn").on("click", function () {})
+$('.saveBtn').on('click', function() {
+    let textareaValue = $('.textarea').val();
+    localStorage.setItem('textareaValue', textareaValue);
+});
+
+//FUNCTION TO GET CURRENT TIME - this shows the hour in terms of which hour in 24 hour clock time, so 8pm is hour 20 
+//function timeTracker() {
+    let timeNow = moment().hour();
+    //}
+    //timeTracker();
+    console.log(timeNow);
+
 
 //PSEUDOCODE:
 // when you type text in the middle column, the value of that text needs to be saved to local storage
@@ -10,9 +21,4 @@ $(".saveBtn").on("click", function () {})
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed: use the time now function to get the current time, if the current time is in the future then background is set to green, if its current, leave it blank, if its in the past, then set the background to red. 
 // you need to give each row a value, and then do if statements that assess the 'timenow' against the value of the the row... (?)
 
-//FUNCTION TO GET CURRENT TIME - this shows the hour in terms of which hour in 24 hour clock time, so 8pm is hour 20 
-//function timeTracker() {
-let timeNow = moment().hour();
-//}
-//timeTracker();
-console.log(timeNow);
+
